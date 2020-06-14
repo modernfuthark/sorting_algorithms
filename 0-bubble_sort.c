10334284c1229e34ca93caa7ee1620829d2a4477
modernfuthark <timothys1738@gmail.com>
2020-06-14 02:39:47 -0400

    [N] Added files for task 0 to directory

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Bubble sorting algorithm
+ * @array: Unsorted array
+ * @size: Size of array
+ * Return: Nothing, function is void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int swappy = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swappy = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swappy;
+			}
+		}
+		print_array(array, size);
+	}
+}
